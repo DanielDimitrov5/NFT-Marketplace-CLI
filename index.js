f79e5dafda6475726d07a421d4f9db1a5feaab83
DanielDimitrov5 <danieldimitrov2304@gmail.com>
2023-07-04 19:30:51 +0300

    additional functions

--- index.js
+++ index.js
@@ -8,41 +8,33 @@ import figlet from "figlet";
 import { createSpinner } from "nanospinner";
 import { ethers } from "ethers";
 import NFTMarketplaceSDK from "nft-mp-sdk";
+import fs from "fs";
 
 import nftMarketplaceABI from "./contractData/abi/NFTMarketplace.json" assert { type: "json" };
 import nftABI from "./contractData/abi/NFT.json" assert { type: "json" };
 import nftBytecode from "./contractData/NftBytecode.json" assert { type: "json" };
 
 let contractAddress;
-let sdkInstance;
 let privateKey;
+let account;
+
+let projectId;
+let projectSecret;
+
+let sdkInstance;
 
 const spleep = (ms = 2000) => new Promise((resolve) => setTimeout(resolve, ms));
 
 async function welcome() {
     const rainbow = chalkAnimation.karaoke("Welcome to the NFT-Marketplace CLI");
 
-    await spleep(100);
+    await spleep(2000);
     rainbow.stop();
 }
 
 await welcome();
 
-function handleInputContract() {
-    let provider = new ethers.providers.InfuraProvider("sepolia", "09755767452a49d3a5b3f9b84d9db6c9");
-
-    if (privateKey) {
-        const wallet = new ethers.Wallet(privateKey);
-
-        provider = wallet.connect(provider);
-    }
-
-    const sdk = new NFTMarketplaceSDK(provider, contractAddress, nftMarketplaceABI, nftABI, nftBytecode.bytecode, 'https://charity-file-storage.infura-ipfs.io/ipfs/');
-
-    sdkInstance = sdk;
-}
-
-async function askForData() {
+async function provideContractAddress() {
     const { contract } = await inquirer.prompt([
         {
             type: "input",
@@ -52,22 +44,75 @@ async function askForData() {
         },
     ]);
 
+    if (!ethers.utils.isAddress(contract)) {
+        console.log(chalk.bgRed("Invalid contract address"));        
+        await provideContractAddress();
+        return;
+    }
+
+    contractAddress = contract;
+}
+
+await provideContractAddress();
+
+async function providerPrivateKey() {
     const { privateKey: key } = await inquirer.prompt([
         {
             type: "input",
             name: "privateKey",
-            message: "Enter your wallet address:",
+            message: "Enter your private key:",
         },
     ]);
 
-    contractAddress = contract;
-
-    privateKey = key;
+    privateKey = key.trim();
 
     handleInputContract();
 }
 
-await askForData();
+await providerPrivateKey();
+
+async function providePrivateKeyRequired() {
+    const { privateKey: key } = await inquirer.prompt([
+        {
+            type: "input",
+            name: "privateKey",
+            message: "Enter your private key:",
+        },
+    ]);
+
+    if (!verify(key)) {
+        console.log(chalk.bgRed("Invalid private key"));
+        await providePrivateKeyRequired();
+        return;
+    }
+
+    privateKey = key.trim();
+
+    handleInputContract();
+}
+
+function handleInputContract() {
+    let provider = new ethers.providers.InfuraProvider("sepolia", "09755767452a49d3a5b3f9b84d9db6c9");
+
+    if (verify(privateKey)) {
+        const wallet = new ethers.Wallet(privateKey);
+
+        provider = wallet.connect(provider);
+
+        account = wallet.address;
+    }
+
+    const sdk = new NFTMarketplaceSDK(provider, contractAddress, nftMarketplaceABI, nftABI, nftBytecode.bytecode, 'https://charity-file-storage.infura-ipfs.io/ipfs/');
+
+    sdkInstance = sdk;
+}
+
+function verify(value) {
+    try {
+        new ethers.Wallet(value);
+    } catch (e) { return false; }
+    return true;
+}
 
 async function showItems() {
     const spinner = createSpinner("Loading items...");
@@ -140,7 +185,7 @@ async function buyItem() {
         };
     });
 
-    const filteredItems = itemsWithMetadata.filter((item) => item.price.toNumber() != 0).map(item => {
+    const filteredItems = itemsWithMetadata.filter((item) => item.price.toString() != "0" && item.owner !== account).map(item => {
         return {
             id: item.id,
             name: item.data.name,
@@ -149,6 +194,11 @@ async function buyItem() {
             owner: item.owner,
         }
     });
+
+    if (filteredItems.length == 0) {
+        console.log(chalk.bgRed("There are no items to buy"));
+        return;
+    }
 
     const choices = filteredItems.map((item) => {
         return {
@@ -173,7 +223,7 @@ async function buyItem() {
 
     const owner = item.owner;
 
-    if (owner == sdkInstance.signerOrProvider.address) {
+    if (owner == account) {
         spinner.stop();
         console.log(chalk.bgRed("You can't buy an item you own!"));
         return;
@@ -202,7 +252,7 @@ async function makeOffer() {
         };
     });
 
-    const filteredItems = itemsWithMetadata.filter((item) => item.price.toNumber() == 0 && item.owner != sdkInstance.signerOrProvider.address).map(item => {
+    const filteredItems = itemsWithMetadata.filter((item) => item.price.toString() == "0" && item.owner != account).map(item => {
         return {
             id: item.id,
             name: item.data.name,
@@ -256,7 +306,7 @@ async function makeOffer() {
 }
 
 async function MyOffers() {
-    const offers = await sdkInstance.getAccountsOffers(sdkInstance.signerOrProvider.address);
+    const offers = await sdkInstance.getAccountsOffers(account);
 
     const choices = offers.map((offer) => {
         return {
@@ -317,7 +367,7 @@ async function MyOffers() {
 async function acceptOffer() {
     const { items } = await sdkInstance.loadItems();
 
-    const filteredItems = items.filter((item) => item.owner == sdkInstance.signerOrProvider.address && item.price.toNumber() == 0);
+    const filteredItems = items.filter((item) => item.owner == account && item.price.toString() == "0");
 
     const ids = filteredItems.map((item) => item.id.toNumber());
 
@@ -325,7 +375,7 @@ async function acceptOffer() {
 
     const offerPromises = idArr.map((id) => sdkInstance.getOffers(id));
 
-    const offers = await Promise.all(offerPromises); 
+    const offers = await Promise.all(offerPromises);
 
     offers.forEach((offer, index) => {
         offer.forEach((o) => {
@@ -333,12 +383,17 @@ async function acceptOffer() {
         });
     });
 
-    const offersFiltered = offers.filter((offer) => offer.length > 0).flat().filter((offer) => offer.seller === sdkInstance.signerOrProvider.address && offer.isAccepted == false);
+    const offersFiltered = offers.filter((offer) => offer.length > 0).flat().filter((offer) => offer.seller === account && offer.isAccepted == false);
+
+    if (offersFiltered.length == 0) {
+        console.log(chalk.bgRed("You don't have any offers to accept!"));
+        return;
+    }
 
     const choices = offersFiltered.map((offer) => {
         return {
             name: `ID: ${offer.itemId} - From: ${offer.offerer} - ${ethers.utils.formatEther(offer.price)} ETH`,
-            value: {id: offer.itemId, offerer: offer.offerer},
+            value: { id: offer.itemId, offerer: offer.offerer },
             price: offer.price,
             nftContract: offer.nftContract,
             tokenId: offer.tokenId,
@@ -368,15 +423,252 @@ async function acceptOffer() {
     }
 }
 
+async function loadCollections() {
+    const collections = await sdkInstance.loadCollections();
+
+    const ownersPromises = collections.map((collection) => {
+        const contract = new ethers.Contract(collection.address, sdkInstance.nftABI, sdkInstance.signerOrProvider);
+        return contract.owner();
+    });
+
+    const owners = await Promise.all(ownersPromises);
+
+    const filteredCollections = collections.filter((collection, index) => owners[index] == account);
+
+    const choices = filteredCollections.map((collection) => {
+        return {
+            name: `${collection.name} - ${collection.symbol} - ${collection.address}`,
+            value: collection.address,
+        };
+    });
+
+    return choices;
+}
+
+async function provideIdSecter() {
+    if (!projectId || !projectSecret) {
+        console.log(chalk.bgRed("Please provide a project ID and project secret!"));
+
+        const { id } = await inquirer.prompt([
+            {
+                type: "input",
+                name: "id",
+                message: "Enter your project ID:",
+            },
+        ]);
+
+        const { secret } = await inquirer.prompt([
+            {
+                type: "input",
+                name: "secret",
+                message: "Enter your project secret:",
+            },
+        ]);
+
+        projectId = id;
+        projectSecret = secret;
+    }
+}
+
+async function mintItem() {
+
+    await provideIdSecter();
+
+    const collections = await loadCollections();
+
+    if (collections.length == 0) {
+        console.log(chalk.bgRed("You don't own any collections!"));
+        return;
+    }
+
+    const { collection } = await inquirer.prompt([
+        {
+            type: "list",
+            name: "collection",
+            message: "Select the collection you want to mint an item from:",
+            choices: collections,
+        },
+    ]);
+
+    const { name } = await inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Enter the name of the item:",
+        },
+    ]);
+
+    const { description } = await inquirer.prompt([
+        {
+            type: "input",
+            name: "description",
+            message: "Enter the description of the item:",
+        },
+    ]);
+
+    const { image } = await inquirer.prompt([
+        {
+            type: "input",
+            name: "image",
+            message: "Enter the path of the image of the item:",
+            default: "C:\\Users\\DanielDimitrov\\OneDrive - INENSIA\\Desktop\\mem.png",
+        },
+    ]);
+
+    const img = fs.readFileSync(image);
+
+    const spinner = createSpinner("Minting item...");
+    spinner.start();
+
+    const metadata = {
+        name: name,
+        description: description,
+        image: img,
+    }
+
+    const mintClient = await sdkInstance.infuraIpfsClient(projectId, projectSecret);
+
+    const result = await mintClient.mintNFT(collection, metadata);
+
+    if (result == 1) {
+        spinner.stop();
+        console.log(chalk.bgCyan("Item minted successfully!"));
+    }
+    else {
+        console.log(chalk.bgRed("Something went wrong!"));
+    }
+}
+
+async function addItemsToMarketplace() {
+    const collections = await loadCollections();
+
+    if (collections.length == 0) {
+        console.log(chalk.bgRed("You don't own any collections!"));
+        return;
+    }
+
+    const { collection } = await inquirer.prompt([
+        {
+            type: "list",
+            name: "collection",
+            message: "Select the collection you want to add an item from:",
+            choices: collections,
+        },
+    ]);
+
+    const itemsForAdding = await sdkInstance.loadItemsForAdding(collection, account);
+
+    if (itemsForAdding.length == 0) {
+        console.log(chalk.bgRed("You don't own any items that are not in the marketplace!"));
+        return;
+    }
+
+    const choices = itemsForAdding.map((item) => {
+        return {
+            name: `ID: ${item.tokenId.toString()} - ${item.name} - ${item.description} | ${item.image}`,
+            value: item.tokenId,
+        };
+    });
+
+    const { id } = await inquirer.prompt([
+        {
+            type: "list",
+            name: "id",
+            message: "Select the item you want to add to the marketplace:",
+            choices: choices,
+        },
+    ]);
+
+    const spinner = createSpinner("Adding item to marketplace...");
+    spinner.start();
+
+    const result = await sdkInstance.addItemToMarketplace(collection, id);
+
+    if (result == 1) {
+        spinner.stop();
+        console.log(chalk.bgCyan("Item added to marketplace successfully!"));
+    }
+    else {
+        console.log(chalk.bgRed("Something went wrong!"));
+    }
+}
+
+async function listItem() {
+    const { filteredItems: items, nfts } = await sdkInstance.loadItemsForListing(account);
+
+    const combinedItems = items.map((item) => {
+        const nft = nfts.find((nft) => nft.tokenId == item.tokenId);
+        return {
+            ...item,
+            name: nft.name,
+            description: nft.description,
+            image: nft.image,
+        };
+    });
+
+    if (items.length == 0) {
+        console.log(chalk.bgRed("You don't own any items that are not listed!"));
+        return;
+    }
+
+    const choices = combinedItems.map((item) => {
+        return {
+            name: `ID: ${item.tokenId.toString()} - ${item.name} - ${item.description} | ${item.image}`,
+            value: { id: item.tokenId, nft: item.nftContract },
+        };
+    });
+
+    const { data } = await inquirer.prompt([
+        {
+            type: "list",
+            name: "data",
+            message: "Select the item you want to list:",
+            choices: choices,
+        },
+    ]);
+
+    const { price } = await inquirer.prompt([
+        {
+            type: "input",
+            name: "price",
+            message: "Enter the price(ETH) of the item:",
+        },
+    ]);
+
+    const spinner = createSpinner("Listing item...");
+    spinner.start();
+
+    const result = await sdkInstance.listItemForSale(data.nft, data.id, ethers.utils.parseEther(price));
+
+    if (result == 1) {
+        spinner.stop();
+        console.log(chalk.bgCyan("Item listed successfully!"));
+    }
+    else {
+        console.log(chalk.bgRed("Something went wrong!"));
+    }
+
+    spinner.stop();
+}
+
 async function askForOption() {
     const { option } = await inquirer.prompt([
         {
             type: "list",
             name: "option",
             message: "What do you want to do?",
-            choices: ["Show all items", "Show item", "Buy item", "Make offer", "My offers", "Accept offer", "Exit"],
+            choices: ["Show all items", "Show item", "Buy item", "Mint item", "Add item to marketplace", "List item", "Make offer", "My offers", "Accept offer", "Exit"],
         },
     ]);
+
+    if (option !== "Exit" && option !== "Show all items" && option !== "Show item") {
+        if (!account) {
+            console.log(chalk.bgRed("You must provide private key!"));
+            await providePrivateKeyRequired();
+
+            handleInputContract();
+        }
+    }
 
     if (option === "Show all items") {
         await showItems();
@@ -385,6 +677,15 @@ async function askForOption() {
     }
     else if (option === "Buy item") {
         await buyItem();
+    }
+    else if (option === "Mint item") {
+        await mintItem();
+    }
+    else if (option === "Add item to marketplace") {
+        await addItemsToMarketplace();
+    }
+    else if (option === "List item") {
+        await listItem();
     }
     else if (option === "Make offer") {
         await makeOffer();
@@ -396,6 +697,7 @@ async function askForOption() {
         await acceptOffer();
     }
     else if (option === "Exit") {
+        console.clear();
         process.exit();
     }
 
